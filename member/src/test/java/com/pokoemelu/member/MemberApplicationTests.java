5692b1f488266379563c9410d7bc665a6dd60eaf
Akhmad Miftah <ext.akhmad@jalin.co.id>
2025-05-01 10:36:25 +0700

    First commit

---
+++ member/src/test/java/com/pokoemelu/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokoemelu.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
